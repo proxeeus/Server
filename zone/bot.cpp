31dfc0e3817743b037197b7b05aae65d1265bd6d
proxeeus <proxeeus@gmail.com>
2017-09-26 20:50:19 +0200

    manual fix for untargettable bots manual fix for zone.exe crash on out of range bot

--- zone/bot.cpp
+++ zone/bot.cpp
@@ -2842,6 +2842,7 @@ void Bot::Spawn(Client* botCharacterOwner) {
 		FaceTarget(botCharacterOwner);
 		UpdateEquipmentLight();
 		UpdateActiveLight();
+		this->m_targetable = true;
 		entity_list.AddBot(this, true, true);
 		// Load pet
 		LoadPet();
