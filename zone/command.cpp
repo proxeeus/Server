15b1645d11004fa6ebf014af37951d45f402bf6b
proxeeus <proxeeus@gmail.com>
2017-12-09 17:24:25 +0100

    enhanced #spawnfix: now if no records exist, a brand new spawngroup/entry/spawn2 will be created.

--- zone/command.cpp
+++ zone/command.cpp
@@ -4335,6 +4335,67 @@ void command_spawnfix(Client *c, const Seperator *sep) {
 
     if(!s2) {
         c->Message(0, "#spawnfix FAILED -- cannot determine which spawn entry in the database this mob came from.");
+		c->Message(0, "Creating a new spawn from scratch...");
+		std::string getMaxSpawngroupIds = "SELECT id FROM spawngroup ORDER BY id DESC LIMIT 0, 1";
+		auto results = database.QueryDatabase(getMaxSpawngroupIds);
+		auto lastSpawnGroupId = 0;
+		if (results.Success())
+		{
+			auto row = results.begin();
+			lastSpawnGroupId = atoi(row[0]);
+		}
+		else
+		{
+			c->Message(13,"Error getting last spawngroup id, exiting command.");
+			return;
+		}
+		c->Message(10, "Inserting new spawngroup...");
+
+		std::string spawngroupquery = StringFormat("insert into spawngroup(name, spawn_limit, dist, max_x, min_x, max_y, min_y, delay, mindelay, despawn, despawn_timer) values('spfix_%s_%i', '0','0','0','0','0','0', '0', '15000','0','100');", zone->GetShortName(), lastSpawnGroupId + 1);
+
+		auto spawngroupResults = database.QueryDatabase(spawngroupquery);
+		if (!spawngroupResults.Success())
+		{
+			c->Message(13, "Error inserting spawngroup, exiting command.");
+			return;
+		}
+
+		auto newSpawnGroupResult = database.QueryDatabase(getMaxSpawngroupIds);
+		if (newSpawnGroupResult.Success())
+		{
+			auto row = newSpawnGroupResult.begin();
+			lastSpawnGroupId = atoi(row[0]);
+		}
+		else
+		{
+			c->Message(13, "Error getting last spawngroup id, exiting command.");
+			return;
+		}
+
+		c->Message(10, "Inserting new spawnentry...");
+		std::string spawnentryquery = StringFormat("insert into spawnentry values('%i', '%i', '100');", lastSpawnGroupId, targetMob->CastToNPC()->GetNPCTypeID());
+		auto spawnentryresults = database.QueryDatabase(spawnentryquery);
+		if (!spawnentryresults.Success())
+		{
+			c->Message(13, "Error inserting sppawnentry.");
+			return;
+		}
+
+		c->Message(10, "Inserting spawn2 entry...");
+
+		std::string spawn2entryquery = StringFormat("insert into spawn2(spawngroupID, zone, version,x,y,z,heading,respawntime,variance,pathgrid,_condition, cond_value,enabled,animation) values('%i','%s','0','%f','%f','%f','%f','0','0','0','0','1','1','0');", lastSpawnGroupId, zone->GetShortName(), c->GetX(), c->GetY(), c->GetZ(), c->GetHeading());
+		auto spawn2entryResults = database.QueryDatabase(spawn2entryquery);
+		if (spawn2entryResults.Success())
+		{
+			c->Message(0, "Spawngroup added successfully.");
+			targetMob->Depop(false);
+		}
+		else
+		{
+			c->Message(13, "Error inserting spawn2 entry, exiting command.");
+			return;
+		}
+
         return;
     }
 
