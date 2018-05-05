6c51023b7df638686fc70eefa7a6b7adc63084f7
proxeeus <proxeeus@gmail.com>
2018-05-05 19:56:41 +0200

    hardcoded bind affinity for bots

--- zone/bot_command.cpp
+++ zone/bot_command.cpp
@@ -2553,40 +2553,49 @@ void bot_command_attack(Client *c, const Seperator *sep)
 
 void bot_command_bind_affinity(Client *c, const Seperator *sep)
 {
-	bcst_list* local_list = &bot_command_spells[BCEnum::SpT_BindAffinity];
-	if (helper_spell_list_fail(c, local_list, BCEnum::SpT_BindAffinity) || helper_command_alias_fail(c, "bot_command_bind_affinity", sep->arg[0], "bindaffinity"))
-		return;
+	// Hardcoding bind affinity
 	if (helper_is_help_or_usage(sep->arg[1])) {
 		c->Message(m_usage, "usage: (<friendly_target>) %s", sep->arg[0]);
 		helper_send_usage_required_bots(c, BCEnum::SpT_BindAffinity);
 		return;
 	}
 
+	auto target_mob = ActionableTarget::AsSingle_ByPlayer(c);
+	if (!target_mob)
+		return;
+
 	ActionableTarget::Types actionable_targets;
 	Bot* my_bot = nullptr;
 	std::list<Bot*> sbl;
 	MyBots::PopulateSBL_BySpawnedBots(c, sbl);
 
-	for (auto list_iter : *local_list) {
-		auto local_entry = list_iter;
-		if (helper_spell_check_fail(local_entry))
-			continue;
-
-		auto target_mob = actionable_targets.Select(c, local_entry->target_type, FRIENDLY);
-		if (!target_mob)
-			continue;
-
-		my_bot = ActionableBots::Select_ByMinLevelAndClass(c, local_entry->target_type, sbl, local_entry->spell_level, local_entry->caster_class, target_mob);
-		if (!my_bot)
-			continue;
-
-		// Cast effect message is not being generated
-		if (helper_cast_standard_spell(my_bot, target_mob, local_entry->spell_id))
-			c->Message(m_action, "Successfully bound %s to this location", target_mob->GetCleanName());
-		else
-			c->Message(m_fail, "Failed to bind %s to this location", target_mob->GetCleanName());
-		break;
+	my_bot = ActionableBots::AsSpawned_ByMinLevelAndClass(c, sbl, 12, ENCHANTER);
+	if (!my_bot) {
+		my_bot = ActionableBots::AsSpawned_ByMinLevelAndClass(c, sbl,12, MAGICIAN);
 	}
+	if (!my_bot) {
+		my_bot = ActionableBots::AsSpawned_ByMinLevelAndClass(c, sbl, 12, NECROMANCER);
+	}
+	if (!my_bot) {
+		my_bot = ActionableBots::AsSpawned_ByMinLevelAndClass(c, sbl, 12, WIZARD);
+	}
+	if (!my_bot) {
+		my_bot = ActionableBots::AsSpawned_ByMinLevelAndClass(c, sbl, 14, SHAMAN);
+	}
+	if (!my_bot) {
+		my_bot = ActionableBots::AsSpawned_ByMinLevelAndClass(c, sbl, 14, DRUID);
+	}
+	if (!my_bot) {
+		my_bot = ActionableBots::AsSpawned_ByMinLevelAndClass(c, sbl, 12, CLERIC);
+	}
+	if (!my_bot) {
+		c->Message(m_fail, "No currently spawned bots are able to cast an invisibility spell.");
+		return;
+	}
+	if(helper_cast_standard_spell(my_bot, target_mob, 35))
+		c->Message(m_action, "Successfully bound %s to this location", target_mob->GetCleanName());
+	else
+		c->Message(m_fail, "Failed to bind %s to this location", target_mob->GetCleanName());
 	
 	helper_no_available_bots(c, my_bot);
 }
