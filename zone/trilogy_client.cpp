cf2319e65b33736e77d916c8e1dd70ca0e5fa6cd
proxeeus <proxeeus@gmail.com>
2026-05-30 12:29:51 +0200

    remove diagnostic log

--- zone/trilogy_client.cpp
+++ zone/trilogy_client.cpp
@@ -1487,12 +1487,6 @@ void TrilogyClient::HandleGroundSpawn(const EQApplicationPacket* app)
 		*reinterpret_cast<int16_t*>(buf + 238) = OT_DROPPEDITEM;
 	}
 
-	const bool is_container = (emu->object_type >= ObjectTypes::ToolBox);
-	LogInfo("[TrilogyClient] HandleGroundSpawn: name='{}' obj_type={} container={} drop_id={} pos=({:.1f},{:.1f},{:.1f}) {} (deferred_q={})",
-	        emu->object_name, static_cast<int>(emu->object_type), is_container,
-	        emu->drop_id, emu->x, emu->y, emu->z,
-	        m_is_zoning ? "deferred" : "sent", m_deferred_spawns.size());
-
 	if (m_is_zoning) {
 		if (m_deferred_spawns.size() < kMaxDeferredSpawns) {
 			m_deferred_spawns.emplace_back(uint16_t{0x3520},
